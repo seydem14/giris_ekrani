a5c0c0977a42328d6dea59f9830d56e8e64b2aff
seydem14 <seymanurdemirbas14@gmail.com>
2023-04-06 04:28:44 +0300

    giriş ekranı

---
+++ Asp.Net Core MVC Login Form Using Ms SQL C#/EmployeeManager/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeManager.Data;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            EmpoyeeModel _empoyeeModel = new EmpoyeeModel();
+            return View(_empoyeeModel);
+        }
+        [HttpPost]
+        public IActionResult Index(EmpoyeeModel _empoyeeModel)
+        {
+            EmployeeContext _employeeContext = new EmployeeContext();
+            var status = _employeeContext.EmployeeLogin.Where(m => m.LoginId == _empoyeeModel.LoginId && m.Password == _empoyeeModel.Pasword).FirstOrDefault();
+            if (status == null)
+            {
+                ViewBag.LoginStatus = 0;
+            }
+            else
+            {
+                return RedirectToAction("SuccessPage", "Home");
+            }
+            return View(_empoyeeModel);
+        }
+        public IActionResult SuccessPage()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net Core MVC Login Form Using Ms SQL C#/EmployeeManager/Data/EmployeeContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeManager.Data
+{
+    public partial class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeLogin> EmployeeLogin { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-JI65VED\\SQLEXPRESS;Database=Employee;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeLogin>(entity =>
+            {
+                entity.Property(e => e.EmpoyeeName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LoginId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
